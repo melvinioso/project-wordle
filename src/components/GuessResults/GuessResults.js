af8931bf42ad28984c06df9d35f2b41e7b21ae03
Matt McGavern <matt.mcgavern@gmail.com>
2024-08-21 19:51:43 -0500

    exercise 3 - guess slots

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,17 @@
 import React from "react"
 
+import Guess from "../Guess"
+import { range } from "../../utils"
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants"
+
 function GuessResults({ guessResults }) {
+  const maxGuesses = range(0, NUM_OF_GUESSES_ALLOWED, 1)
+  const guesses = maxGuesses.map((guessIndex) => guessResults[guessIndex] || "")
+
   return (
     <div className="guess-results">
-      {guessResults.map((result, i) => (
-        <p key={i} className="guess">
-          {result}
-        </p>
+      {guesses.map((guess, index) => (
+        <Guess key={index} guess={guess} />
       ))}
     </div>
   )
